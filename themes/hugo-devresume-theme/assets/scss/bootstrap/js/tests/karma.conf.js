c87bc8610bc92ea65b0b137dbd5ddc4f0121b49c
Alejandro López <alejandrolopez.gd@gmail.com>
2021-10-10 10:25:14 -0300

    chore: cleanup unused files and folders

--- themes/hugo-devresume-theme/assets/scss/bootstrap/js/tests/karma.conf.js
+++ themes/hugo-devresume-theme/assets/scss/bootstrap/js/tests/karma.conf.js
@@ -1,33 +1,28 @@
-/* eslint-env node */
 /* eslint no-process-env: 0 */
 
 const path = require('path')
 const ip = require('ip')
-const {
-  browsers,
-  browsersKeys
-} = require('./browsers')
+const { browsers, browsersKeys } = require('./browsers')
 
-const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'node_modules/jquery/dist/jquery.slim.min.js'
+const jqueryFile = process.env.USE_OLD_JQUERY
+  ? 'https://code.jquery.com/jquery-1.9.1.min.js'
+  : 'node_modules/jquery/dist/jquery.slim.min.js'
 const bundle = process.env.BUNDLE === 'true'
 const browserStack = process.env.BROWSER === 'true'
 
-const frameworks = [
-  'qunit',
-  'sinon'
-]
+const frameworks = ['qunit', 'sinon']
 
-const plugins = [
-  'karma-qunit',
-  'karma-sinon'
-]
+const plugins = ['karma-qunit', 'karma-sinon']
 
 const reporters = ['dots']
 
 const detectBrowsers = {
   usePhantomJS: false,
   postDetection(availableBrowser) {
-    if (typeof process.env.TRAVIS_JOB_ID !== 'undefined' || availableBrowser.includes('Chrome')) {
+    if (
+      typeof process.env.TRAVIS_JOB_ID !== 'undefined' ||
+      availableBrowser.includes('Chrome')
+    ) {
       return ['ChromeHeadless']
     }
 
@@ -36,19 +31,19 @@ const detectBrowsers = {
     }
 
     throw new Error('Please install Firefox or Chrome')
-  }
+  },
 }
 
 const customLaunchers = {
   FirefoxHeadless: {
     base: 'Firefox',
-    flags: ['-headless']
-  }
+    flags: ['-headless'],
+  },
 }
 
 let files = [
   'node_modules/popper.js/dist/umd/popper.min.js',
-  'node_modules/hammer-simulator/index.js'
+  'node_modules/hammer-simulator/index.js',
 ]
 
 const conf = {
@@ -60,9 +55,9 @@ const conf = {
   concurrency: Infinity,
   client: {
     qunit: {
-      showUI: true
-    }
-  }
+      showUI: true,
+    },
+  },
 }
 
 if (bundle) {
@@ -74,10 +69,7 @@ if (bundle) {
   )
   conf.customLaunchers = customLaunchers
   conf.detectBrowsers = detectBrowsers
-  files = files.concat([
-    jqueryFile,
-    'dist/js/bootstrap.js'
-  ])
+  files = files.concat([jqueryFile, 'dist/js/bootstrap.js'])
 } else if (browserStack) {
   conf.hostname = ip.address()
   conf.browserStack = {
@@ -85,7 +77,7 @@ if (bundle) {
     accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
     build: `bootstrap-${new Date().toISOString()}`,
     project: 'Bootstrap',
-    retryLimit: 2
+    retryLimit: 2,
   }
   plugins.push('karma-browserstack-launcher')
   conf.customLaunchers = browsers
@@ -95,7 +87,7 @@ if (bundle) {
     'node_modules/jquery/dist/jquery.slim.min.js',
     'js/dist/util.js',
     'js/dist/tooltip.js',
-    'js/dist/!(util|index|tooltip).js' // include all of our js/dist files except util.js, index.js and tooltip.js
+    'js/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
   ])
 } else {
   frameworks.push('detectBrowsers')
@@ -109,7 +101,7 @@ if (bundle) {
     jqueryFile,
     'js/coverage/dist/util.js',
     'js/coverage/dist/tooltip.js',
-    'js/coverage/dist/!(util|index|tooltip).js' // include all of our js/dist files except util.js, index.js and tooltip.js
+    'js/coverage/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
   ])
   reporters.push('coverage-istanbul')
   conf.customLaunchers = customLaunchers
@@ -123,9 +115,9 @@ if (bundle) {
         statements: 90,
         branches: 86,
         functions: 89,
-        lines: 90
-      }
-    }
+        lines: 90,
+      },
+    },
   }
 }
 
